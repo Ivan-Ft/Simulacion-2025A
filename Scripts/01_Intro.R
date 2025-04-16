56476d2d1b1ae0dcdf067519f1728c68e430fb9d
Ivan-Ft <ivanfuertes99@gmail.com>
2025-04-16 15:43:21 +0000

    Rprofile

--- Scripts/01_Intro.R
+++ Scripts/01_Intro.R
@@ -1,4 +1,25 @@
 # Primer script del curso
+#Configuraciones Iniciales
+179/5378641
+options(scipen = 999)
+options(digits = 2)
+
+#Instalacion de paquetes
+install.packages("data.table")
+install.packages("tidyverse", dependencies = TRUE)
+library(data.table)
+library(tidyverse)
+
+#Rprofile
+if(!file.exists("~/.Rprofile")){
+  file.create("~/.Rprofile")
+}else {
+  file.edit("~/.Rprofile")
+}
+
+
+
+
 
 # Vectores
 c(2,5,8,12)
@@ -8,3 +29,5 @@ mean(c(2,5,8,12))
 x <- c(2,5,8,12)
 sum(x)
 mean(x)
+
+x %>% sum()
